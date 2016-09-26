22df25ccbcfb617a228d56937d784bd939b4d874
Milwy <Milwy@Zikora>
2016-09-25 20:47:45 -0400

    Added support for ie, ff48+// working on fixing edge

--- src/Browser.java
+++ src/Browser.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -10,32 +13,52 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Browser {
 
-	static int whichBrowser = 1; //1 chrome, 2 ff, 3 edge
-
-	static WebDriver driver;
-	static Wait<WebDriver> wait;
-
+	private static int whichBrowser = 2; //1 chrome, 2 ff, 3 edge, 4 IE 
+	private static WebDriver driver;
+	private static Wait<WebDriver> wait;
 
 	public static void main(String[] args) {
+		new Browser();
+	}
+	
+	public Browser(){
+		setupBrowserDriver();
+		wait = new WebDriverWait(driver, 30);
+        driver.get("http://www.google.com/");
+        //visit_forums();
+	}
+	
+	private void downloadDrivers(){
+		
+	}
+	
+	private void setupBrowserDriver(){
 		switch (whichBrowser){
 		case 1:
+			//https://sites.google.com/a/chromium.org/chromedriver/downloads
 			System.setProperty("webdriver.chrome.driver", "C:/Users/Milwy/githubStuff/SeleniumTest/drivers/chromedriver.exe");
 			driver = new ChromeDriver();
 			break;
-			
 		case 2:
-			driver = new FirefoxDriver();
+			//https://github.com/mozilla/geckodriver/releases/tag/v0.10.0
+			System.setProperty("webdriver.gecko.driver", "C:/Users/Milwy/githubStuff/SeleniumTest/drivers/geckodriver.exe");
+			driver = new MarionetteDriver();
 			//add gecko driver for 48+
-			break;
-			
+			break;		
 		case 3:
-			
+			//https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+			if(OSValidator.getOS().compareTo("windows 10")==0){
+				System.out.println("I'm win10");
+				System.setProperty("webdriver.edge.driver", "C:/Users/Milwy/githubStuff/SeleniumTest/drivers/MicrosoftWebDriver.exe");
+				driver = new EdgeDriver();
+			}
+			break;		
+		case 4:
+			//http://www.seleniumhq.org/download/
+			System.setProperty("webdriver.ie.driver", "C:/Users/Milwy/githubStuff/SeleniumTest/drivers/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 			break;
 		}
-		
-		wait = new WebDriverWait(driver, 30);
-        driver.get("http://www.google.com/");
-        visit_forums();
 	}
 	
 	private static void visit_forums(){
